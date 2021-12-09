c0531a201243cb1fe44cc4dd66148107f179f88c
Jacob Miller <jmjacobmiller@gmail.com>
2021-12-09 18:04:31 -0500

    Added plotting and option for making covariance mats bistochastic

--- continuous-umps/grayscale_correlations.py
+++ continuous-umps/grayscale_correlations.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import torch
 import matplotlib.pyplot as plt
 
@@ -10,32 +12,57 @@ from torchmps.embeddings import (
 
 
 @torch.no_grad()
-def make_covariance_mat(embedding_spec, embedding_dim, frameify=True, num_points=256):
+def make_covariance_mat():
     """
     Convert embedding function into pixel-pixel covariance matrix
-
-    Args:
-        embedding_spec: String specifying function taking batches of greyscale
-            values and mapping them to batches of embedded vectors.
-            Must be one of "legendre", "trig", or "binned".
-        embedding_dim: Dimension of the embedded vectors.
-        frameify: Whether or not to convert embedding functions into frames.
-        num_points: The number of grayscale points to evaluate the covariance
-            matrix at.
     """
+    global EMBEDDING_SPEC, EMBEDDING_DIM, FRAMEIFY, NUM_POINTS
+
     # Set up full embedding function, with option for frameified ingredients
-    embedding_spec = embedding_spec.lower()
-    assert embedding_spec in ["legendre", "trig"]
-    embed_map = {"legendre": legendre_embed, "trig": trig_embed}
-    raw_embed = partial(embed_map[embedding_spec], emb_dim=embedding_dim)
-    embed_fun = FixedEmbedding(embed_fun, unit_interval, frameify=frameify)
+    EMBEDDING_SPEC = EMBEDDING_SPEC.lower()
+    assert EMBEDDING_SPEC in ["legendre", "trigonometric"]
+    embed_map = {"legendre": legendre_embed, "trigonometric": trig_embed}
+    raw_embed = partial(embed_map[EMBEDDING_SPEC], emb_dim=EMBEDDING_DIM)
+    embed_fun = FixedEmbedding(raw_embed, unit_interval, frameify=FRAMEIFY)
 
     # Compute all inner products between embedding vectors
-    points = torch.linspace(0.0, 1.0, steps=num_points)
+    points = torch.linspace(0.0, 1.0, steps=NUM_POINTS)
     emb_vecs = embed_fun(points)
-    assert emb_vecs.shape == (num_points, embedding_dim)
+    assert emb_vecs.shape == (NUM_POINTS, EMBEDDING_DIM)
     raw_inner_prods = emb_vecs @ emb_vecs.T.conj()
 
+    # Convert the covariance matrix entries into conditional probabilities
     covariance_mat = raw_inner_prods.abs().square()
-    assert covariance_mat.shape == (num_points, num_points)
+    assert covariance_mat.shape == (NUM_POINTS, NUM_POINTS)
     return covariance_mat
+
+
+if __name__ == "__main__":
+    # Global configuration options:
+    # EMBEDDING_SPEC: String specifying function taking batches of greyscale
+    #     values and mapping them to batches of embedded vectors.
+    #     Must be one of "legendre", "trigonometric", or "binned".
+    # EMBEDDING_DIM: Dimension of the embedded vectors.
+    # FRAMEIFY: Whether or not to convert embedding functions into frames.
+    # NUM_POINTS: The number of grayscale points to evaluate the covariance
+    #     matrix at.
+    # BISTOCHASTIC: Whether to convert the covariance matrix into a conditional
+    #   probability distribution.
+    global EMBEDDING_SPEC, EMBEDDING_DIM, FRAMEIFY, NUM_POINTS
+    EMBEDDING_SPEC = "legendre"
+    EMBEDDING_DIM = 10
+    FRAMEIFY = False
+    NUM_POINTS = 256
+    BISTOCHASTIC = True
+
+    # Prepare covariance matrix
+    covariance_mat = make_covariance_mat()
+    if BISTOCHASTIC:
+        from sinkhorn_knopp import sinkhorn_knopp
+        sk = sinkhorn_knopp.SinkhornKnopp()
+        covariance_mat = sk.fit(covariance_mat.numpy())
+
+    plot_title = f"{EMBEDDING_SPEC.title()} Grayscale-Grayscale Covariance, d={EMBEDDING_DIM}"
+    plt.imshow(covariance_mat, extent=[0, 1, 0, 1])
+    plt.title(plot_title)
+    plt.show()
